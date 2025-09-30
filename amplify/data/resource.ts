cc0392e76c8dbeb2ae921b6236f0fcfd53426e62
Levi Vlasblom <l.vlasblom@student.avans.nl>
2025-09-30 15:51:22 +0200

    Update resource.ts

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -22,7 +22,6 @@ export const data = defineData({
     defaultAuthorizationMode: "apiKey",
     // API Key is used for a.allow.public() rules
     apiKeyAuthorizationMode: {
-      expiresInDays: 30,
       expiresInDays: 130,
     },
   },
